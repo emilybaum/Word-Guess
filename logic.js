bfee33cdab4fd1758c3792c403caecb184c79d28
emilybaum <emily.r.baum@gmail.com>
2019-05-03 00:04:08 -0400

    added array and random word guess logic

---
+++ logic.js
@@ -0,0 +1,79 @@
+// ARRAY OF WORDS
+var gameWords = ["alexander", "hamilton", "duel", "america", "theodosia", "jefferson", "angalica", "writing", "lafayette", "burr", "madison", "eliza", "hercules", "mulligan", "federalist", "washington", "treasury", "george", "reynolds", "peggy", "satisfied", "nonstop", "burn", "kingscollege"];
+var gameWordsLower = gameWords.toLowerCase();
+
+var randomWordGen
+// RANDOM WORD GENERATOR
+// this will load into the console with a random word from the array when randomWord() is typed in
+// indexRandom will return the [i] that can be used for selecting the word from the array
+function randomWord(gameWords) {
+    return gameWords[Math.floor(Math.random() * gameWords.length)] = randomWordGen;
+}
+// Does this call need to be bere?
+randomWord()
+
+
+// CHECK IF INPUT IS A CORRECT LETTER
+function isCorrectGuess(word, letter) {
+    var word;
+    var letter;
+    if (onkeyup.includes(word)) {
+        alert("you pressed the correct key");
+        // document.write(/insert where the letter will be/);
+        // answer is true
+    } else {
+        alert("wrong");
+        // answer is false
+        // will need to decrease score
+    }
+}
+isCorrectGuess()
+
+//     // random word has already been generated, reference that word
+//     // when user clicks a key, save that input into a variable
+//     // compare the user input to the contnets of the word
+//     // if the input is contained in the word, true
+//     // if the input is not contained in the word, false
+//     // clicked word will then display on the screen
+// }
+// // arguments of 2: word, letter
+
+
+
+// // GET BLANKS
+
+// function getBlanks(word) {
+//     for (var i = 0; i < word.length; i++) {
+//         var blanks = //function to make a div with a blank [" _ "]
+//             blanks
+//     }
+// }
+// // calculate number the length of the word
+// // create individual divs for that number with a unique ID
+// // insert in the divs the "_" text using the ID for refernece
+
+
+
+
+
+
+
+
+
+
+// // CHECK IF INPUT IS A CORRECT LETTER
+// document.onkeyup = function isCorrectGuess(e) {
+//     console.log(event.which);
+// };
+
+
+// // converts all strings to lowercase
+// .toLowerCase()
+
+
+
+
+
+
+
+
