54e93f0b841a593d146e88844ec28fafe099dde0
emilybaum <emily.r.baum@gmail.com>
2019-05-03 22:56:09 -0400

    updated funcitons through setupRound

--- logic.js
+++ logic.js
@@ -7,9 +7,8 @@ var randomWordGen;
 // indexRandom will return the [i] that can be used for selecting the word from the array
 function randomWord(gameWords) {
     var random = Math.floor(Math.random() * gameWords.length);
-    return gameWords[random] = randomWordGen;
+    return gameWords[random]
 }
-randomWord()
 
 
 // CHECK IF INPUT IS A CORRECT LETTER
@@ -22,57 +21,70 @@ function isCorrectGuess(word, letter) {
         return false;
     }
 }
-isCorrectGuess()
-
-//     // random word has already been generated, reference that word
-//     // when user clicks a key, save that input into a variable
-//     // compare the user input to the contnets of the word
-//     // if the input is contained in the word, true
-//     // if the input is not contained in the word, false
-//     // clicked word will then display on the screen
-// }
-// // arguments of 2: word, letter
 
 
-
-// // GET BLANKS
-
-// function getBlanks(word) {
+// var isCorrectGuess = function(word, letter) {
 //     for (var i = 0; i < word.length; i++) {
-//         var blanks = //function to make a div with a blank [" _ "]
-//             blanks
+//         if (word[i] === letter) {
+//             return true;
+//         }
 //     }
+//     return false;
 // }
-// // calculate number the length of the word
-// // create individual divs for that number with a unique ID
-// // insert in the divs the "_" text using the ID for refernece
 
 
 
 
+// GET BLANKS
+function getBlanks(word) {
+    // computer generates random word from words array
+    var blanksArray = word.split("");
+    // Looping to generate "_" for each letter in array stored in blanks
+    for (var i = 0; i < blanksArray.length; i++) {
+        blanksArray[i] = "_";
+    }
+    return blanksArray;
+}
 
 
 
+// FILL BLANKS
+function fillBlanks(word, puzzleState, letter) {
+    if (isCorrectGuess(word, letter)) {
+        for (var i = 0; i < word.length; i++) {
+            if (word[i] === letter) {
+                puzzleState[i] = letter;
+            }
+        }
+    }
+    return puzzleState;
+}
 
 
+// SET UP ROUND 
+function setupRound(word) {
+    var setup = {
+        word: word,
+        guessesLeft: 9,
+        wrongGuesses: [],
+        puzzleState: [],
+    }
+    return setup;
+}
 
-// // CHECK IF INPUT IS A CORRECT LETTER
-// document.onkeyup = function isCorrectGuess(e) {
-//     console.log(event.which);
-// };
 
-// // converts all strings to lowercase
-// .toLowerCase()
+// UPDATE ROUND
+// function updateRound(setup, letter) {
 
-//  if (onkeyup.includes(word)) {
-//         alert("you pressed the correct key");
-//         // document.write(/insert where the letter will be/);
-//         // answer is true
-//     } else {
-//         alert("wrong");
-//         // answer is false
-//         // will need to decrease score
-//     }
+//         if (isCorrectGuess(word, letter) === false) {
+//         setup.guessesLeft = --1;
+//         setup.wrongGuesses = ++1;
+//         } else {
+//             fillBlanks();
+//         }    
+// }
+
+
 
 
 
