b80cc84d508c37f799073f18605e4fb54da08a5c
emilybaum <emily.r.baum@gmail.com>
2019-05-05 14:21:31 -0400

    moved all javascript to logic.js

--- logic.js
+++ logic.js
@@ -32,7 +32,7 @@ function isCorrectGuess(word, letter) {
 
 
 
-// 1.3 GET BLANKS
+// 1.3 GET BLANKS ******
 function getBlanks(word) {
     var blanksArray = [];
     for (var i = 0; i < word.length; i++) {
@@ -56,7 +56,7 @@ function fillBlanks(word, puzzleState, letter) {
 }
 
 
-// 1.5 SETUP ROUND 
+// 1.5 SETUP ROUND ******
 function setupRound(word) {
     var setup = {
         word: word,
@@ -142,6 +142,54 @@ function startNewRound(game) {
 
 // 1.12 MY GAME
 var myGame = setupGame(gameWords, 0, 0);
+
+
+
+// USER INTERACTIONS
+
+// logs the object
+console.log(myGame)
+
+var keyPressed;
+document.onkeyup = function (evt) {
+    evt = evt || window.event;
+    var userGuess = evt.keyCode || evt.which;
+    keyPressed = String.fromCharCode(userGuess).toLowerCase();
+    console.log("The " + keyPressed + " key was pressed");
+    return keyPressed;
+}
+isCorrectGuess(myGame.round.word, keyPressed);
+fillBlanks(myGame.round.word, myGame.round.puzzleState, keyPressed);
+// updateRound("need setup input -- this is the whole object?", keyPressed);
+hasWon(myGame.round.puzzleState);
+hasLost(myGame.round.guessesLeft);
+// isEndOfRound("need setup input -- this is the whole object?");
+
+
+
+
+
+// Uses the ramdom word and displays the empty blanks
+var puzzleState = document.getElementById("puzzle-state").innerHTML = myGame.round.puzzleState.join(" ");
+
+// Displays the updated object for wrong guesses from user
+var wrongGuessesText = document.createTextNode(myGame.round.wrongGuesses);
+var wrongGuesses = document.getElementById("wrong-guesses").appendChild(wrongGuessesText);
+
+// Displays the updated object for total wins
+var winsText = document.createTextNode(myGame.wins);
+var wins = document.getElementById("win-counter").appendChild(winsText);
+
+// Displays the updated object for total losses
+var lossesText = document.createTextNode(myGame.losses);
+var losses = document.getElementById("loss-counter").appendChild(lossesText);
+
+// Displays the updated object for number of guesses left
+var guessesLeftText = document.createTextNode(myGame.round.guessesLeft);
+var guessesLeft = document.getElementById("guesses-left").appendChild(guessesLeftText);
+
+
+
 
 
 
